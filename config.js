582ead9e8fb4449b6f1e295045bbc654595902d3
Julian Duque <julianduquej@gmail.com>
2025-03-22 18:55:09 -0400

    update slides for nodecongress

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const REDIS_URL = process.env.REDIS_URL;
 const RATE_LIMIT_ENABLED = process.env.RATE_LIMIT_ENABLED === 'true';
 const RATE_LIMIT_MAX = +process.env.RATE_LIMIT_MAX || 400;
 const RATE_LIMIT_WINDOW = process.env.RATE_LIMIT_WINDOW || '1 minute';
-const HEARTBEAT_TIMEOUT = process.env.HEARTBEAT_TIMEOUT || 3;
+const HEARTBEAT_TIMEOUT = process.env.HEARTBEAT_TIMEOUT || 5;
 const EVENT_ID_LENGTH = +process.env.EVENT_ID_LENGTH || 32;
 const EVENTS_MAX = +process.env.EVENTS_MAX || 32;
 const KAFKA_CLIENT_CERT = process.env.KAFKA_CLIENT_CERT;
